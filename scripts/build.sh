bcc78083d1f91448d764dbfe48207c1caf1fbb48
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-20 16:46:59 +0900

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,5 @@ cd $(dirname $0)/../
 mkdir -p build/scripts
 
 cp ndppd build/
-cp ndppd.1.gz build/
-cp ndppd.conf.5.gz build/
 cp scripts/install.sh build/scripts/
 cp scripts/ndppd build/scripts/
--- scripts/install.sh
+++ scripts/install.sh
@@ -6,12 +6,9 @@ cd $(dirname $0)/../
 
 SBINDIR=/usr/sbin
 INITDDIR=/etc/init.d
-MANDIR=/usr/share/man
 
-mkdir -p ${SBINDIR} ${MANDIR} ${MANDIR}/man1 ${MANDIR}/man5
+mkdir -p ${SBINDIR} ${INITDDIR}
 cp ndppd ${SBINDIR}
 chmod +x ${SBINDIR}/ndppd
 cp scripts/ndppd ${INITDDIR}
 chmod +x ${INITDDIR}/ndppd
-cp ndppd.1.gz ${MANDIR}/man1
-cp ndppd.conf.5.gz ${MANDIR}/man5

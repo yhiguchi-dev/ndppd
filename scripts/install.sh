18a5700e844d9197b211b8466d1ea9f9a9d271bc
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-21 19:21:48 +0900

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,22 +5,20 @@ set -eux
 cd $(dirname $0)
 
 SBIN_DIR=/usr/sbin
-POST_CONFIG_DIR=/config/scripts/post-config.d
+INITD_DIR=/etc/init.d
 CONFIG_DIR=/etc
 
 TARGET_ADDRESS=${TARGET_ADDRESS}
 
 sed -e "s/\${TARGET_ADDRESS}/${TARGET_ADDRESS}/" ndppd.conf.template > ndppd.conf
 
-mkdir -p ${SBIN_DIR} ${POST_CONFIG_DIR} ${CONFIG_DIR}
-cp ndppd ndppd.initscript ${SBIN_DIR}
+mkdir -p ${SBIN_DIR} ${INITD_DIR} ${CONFIG_DIR}
+cp ndppd ${SBIN_DIR}
 chmod 755 ${SBIN_DIR}/ndppd
 chown root:root ${SBIN_DIR}/ndppd
-chmod 755 ${SBIN_DIR}/ndppd.initscript
-chown root:root ${SBIN_DIR}/ndppd.initscript
-cp ndppd.sh ${POST_CONFIG_DIR}
-chmod 755 ${POST_CONFIG_DIR}/ndppd.sh
-chown root:root ${POST_CONFIG_DIR}/ndppd.sh
+cp ndppd.initscript ${INITD_DIR}/ndppd
+chmod 755 ${INITD_DIR}/ndppd
+chown root:root ${INITD_DIR}/ndppd
 cp ndppd.conf ${CONFIG_DIR}
 chmod 644 ${CONFIG_DIR}/ndppd.conf
 chown root:root ${CONFIG_DIR}/ndppd.conf

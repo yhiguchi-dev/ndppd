085f3883ffb1b8cffe1674e5ba608d7b9cdb5ae9
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-21 17:55:48 +0900

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ cd $(dirname $0)/../
 mkdir -p build
 
 cp ndppd build/
-cp ndppd.service build/
 cp ndppd.conf.template build/
+cp scripts/ndppd.initscript build/
+cp scripts/ndppd.sh build/
 cp scripts/install.sh build/
--- scripts/install.sh
+++ scripts/install.sh
@@ -5,20 +5,22 @@ set -eux
 cd $(dirname $0)
 
 SBIN_DIR=/usr/sbin
-SYSTEMD_DIR=/etc/systemd/system
+POST_CONFIG_DIR=/config/scripts/post-config.d
 CONFIG_DIR=/etc
 
 TARGET_ADDRESS=${TARGET_ADDRESS}
 
 sed -e "s/\${TARGET_ADDRESS}/${TARGET_ADDRESS}/" ndppd.conf.template > ndppd.conf
 
-mkdir -p ${SBIN_DIR} ${SYSTEMD_DIR}
-cp ndppd ${SBIN_DIR}
+mkdir -p ${SBIN_DIR} ${POST_CONFIG_DIR} ${CONFIG_DIR}
+cp ndppd ndppd.initscript ${SBIN_DIR}
 chmod 755 ${SBIN_DIR}/ndppd
 chown root:root ${SBIN_DIR}/ndppd
-cp ndppd.service ${SYSTEMD_DIR}
-chmod 644 ${SYSTEMD_DIR}/ndppd.service
-chown root:root ${SYSTEMD_DIR}/ndppd.service
+chmod 755 ${SBIN_DIR}/ndppd.initscript
+chown root:root ${SBIN_DIR}/ndppd.initscript
+cp ndppd.sh ${POST_CONFIG_DIR}
+chmod 755 ${POST_CONFIG_DIR}/ndppd.sh
+chown root:root ${POST_CONFIG_DIR}/ndppd.sh
 cp ndppd.conf ${CONFIG_DIR}
 chmod 644 ${CONFIG_DIR}/ndppd.conf
 chown root:root ${CONFIG_DIR}/ndppd.conf

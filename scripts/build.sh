803711bef6ad32040f3a0208bee352d125e63032
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-22 10:04:05 +0900

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,5 +8,5 @@ mkdir -p build
 
 cp ndppd build/
 cp ndppd.conf.template build/
-cp scripts/ndppd.initscript build/
+cp ndppd.service build/
 cp scripts/install.sh build/
--- scripts/install.sh
+++ scripts/install.sh
@@ -5,20 +5,20 @@ set -eux
 cd $(dirname $0)
 
 SBIN_DIR=/usr/sbin
-INITD_DIR=/etc/init.d
+SYSTEMD_DIR=/etc/systemd/system
 CONFIG_DIR=/etc
 
 TARGET_ADDRESS=${TARGET_ADDRESS}
 
 sed -e "s/\${TARGET_ADDRESS}/${TARGET_ADDRESS}/" ndppd.conf.template > ndppd.conf
 
-mkdir -p ${SBIN_DIR} ${INITD_DIR} ${CONFIG_DIR}
+mkdir -p ${SBIN_DIR} ${SYSTEMD_DIR} ${CONFIG_DIR}
 cp ndppd ${SBIN_DIR}
 chmod 755 ${SBIN_DIR}/ndppd
 chown root:root ${SBIN_DIR}/ndppd
-cp ndppd.initscript ${INITD_DIR}/ndppd
-chmod 755 ${INITD_DIR}/ndppd
-chown root:root ${INITD_DIR}/ndppd
+cp ndppd.service ${SYSTEMD_DIR}/ndppd
+chmod 644 ${SYSTEMD_DIR}/ndppd.service
+chown root:root ${SYSTEMD_DIR}/ndppd.service
 cp ndppd.conf ${CONFIG_DIR}
 chmod 644 ${CONFIG_DIR}/ndppd.conf
 chown root:root ${CONFIG_DIR}/ndppd.conf
